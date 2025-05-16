fafde68f98c86ec47e9fd7894acde474ee60555b
jehad-art <jehad_alhussien@yahoo.com>
2025-05-17 01:51:43 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from extract_config import *
-from structure_config import *
-from analyze_config import *
-from print_config import *
-output = ""
-extract_connfig(output)
-structure(output)
-analyze(output)
-printing(output)
