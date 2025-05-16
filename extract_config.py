b447dc622f5f5cad3e7840abdcae8fcb1541efd7
jehad-art <jehad_alhussien@yahoo.com>
2025-05-17 01:51:19 +0300

    Delete extract_config.py

--- extract_config.py
+++
@@ -1,5 +0,0 @@
-from netmiko import ConnectHandler
-def extract_connfig(output):
-    device = ConnectHandler(device_type='cisco_ios', ip='192.168.1.253', username='cisco', password='cisco')
-    output = device.send_command("show run")
-    device.disconnect() 
