bc3dbe20a7784895bcae6776bab1c59bd4aee293
haha hehe <jehad_alhussien@yahoo.com>
2025-05-18 10:39:36 +0300

    Add CORS middleware to allow frontend access

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from routes.devices import router as device_router
 from settings import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Network Security SaaS")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (for now)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(device_router, prefix="/devices", tags=["Devices"])
 
